6b6096e9a398d789ea0ef8d273306b01d4178250
Santiago Bernhardt <santiago.bernhardt@gmail.com>
2018-06-30 21:23:10 +1200

    don´t need all this anymore

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,56 +1,12 @@
-# set SSL/TLS
+﻿# set SSL/TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # check console rights
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 $adminstatus = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-# handy function to change console opacity
-function Set-ConsoleOpacity{
-    param(
-        [ValidateRange(10,100)]
-        [int]$Opacity
-    )
-
-    # Check if pinvoke type already exists, if not import the relevant functions
-    try {
-        $Win32Type = [Win32.WindowLayer]
-    } catch {
-        $Win32Type = Add-Type -MemberDefinition @'
-            [DllImport("user32.dll")]
-            public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-            [DllImport("user32.dll")]
-            public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-            [DllImport("user32.dll")]
-            public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
-'@ -Name WindowLayer -Namespace Win32 -PassThru
-    }
-
-    # Calculate opacity value (0-255)
-    $OpacityValue = [int]($Opacity * 2.56) - 1
-
-    # Grab the host windows handle
-    $ThisProcess = Get-Process -Id $PID
-    $WindowHandle = $ThisProcess.MainWindowHandle
-
-    # "Constants"
-    $GwlExStyle  = -20;
-    $WsExLayered = 0x80000;
-    $LwaAlpha    = 0x2;
-
-    if($Win32Type::GetWindowLong($WindowHandle,-20) -band $WsExLayered -ne $WsExLayered){
-        # If Window isn't already marked "Layered", make it so
-        [void]$Win32Type::SetWindowLong($WindowHandle,$GwlExStyle,$Win32Type::GetWindowLong($WindowHandle,$GwlExStyle) -bxor $WsExLayered)
-    }
-    # Set transparency
-    [void]$Win32Type::SetLayeredWindowAttributes($WindowHandle,0,$OpacityValue,$LwaAlpha)
-}
-
 # set some console ui values
 $host.ui.RawUI.WindowTitle = "Powershell - StartTime: $(Get-Date -Format h:mm:sss) - AdminMode: $adminstatus - Version: $($PSVersionTable.PSVersion)"
-$host.ui.RawUI.windowsize.width = 170
-$host.ui.RawUI.windowsize.height = 50
-Set-ConsoleOpacity -Opacity 95
 
 # do a nice pop up interaction as powershell is running, you will be surpriced how many times powershell runs without your knowledge
 $a = new-object -comobject wscript.shell
@@ -79,47 +35,25 @@ foreach($module in $PowerShellGet_modules){
   }
 }
 
-# import PsGet based modules
-[hashtable]$PsGet_modules = @{
-  
-}
-foreach($module in $PsGet_modules.keys){
-  Write-Host "Importing module $module" -ForegroundColor Yellow
-  try{
-    Import-Module $module -ErrorAction Stop
-  }
-  catch{
-    try{
-      psget\Install-Module -ModuleUrl ($PsGet_modules.Values | where {$_ -match $module}) -Update
-      Import-Module $module
-    }
-    catch{
-      (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/psget/psget/master/GetPsGet.ps1") | iex
-    }
-  }
-}
-
 Get-Module | select Name,Version
 
 # reminder
 Write-Host "Don't forget to use Powershell-Core, 'pwsh'..."
 
 # inet works?
-if((Test-Connection 1.1.1.1 -Quiet -Count 1) -eq $True){
+if((Test-Connection 9.9.9.9 -Quiet -Count 1) -eq $True){
   cd $env:userprofile\Downloads\
   # do some checks
   iwr -Uri https://raw.githubusercontent.com/sbe-arg/powershell-scripts/master/Update-Notifier-Powershell-Core.ps1 -UseBasicParsing -OutFile .\Update-Notifier-Powershell-Core.ps1
   .\Update-Notifier-Powershell-Core
   iwr -Uri https://raw.githubusercontent.com/sbe-arg/powershell-scripts/master/Update-Notifier-Git-SCM.ps1 -UseBasicParsing -OutFile .\Update-Notifier-Git-SCM.ps1
   .\Update-Notifier-Git-SCM
-  iwr -Uri https://raw.githubusercontent.com/sbe-arg/powershell-scripts/master/Update-Notifier-Atom.ps1 -UseBasicParsing -OutFile .\Update-Notifier-Atom.ps1
-  .\Update-Notifier-Atom
   # this one is different
   iwr -Uri https://raw.githubusercontent.com/sbe-arg/powershell-scripts/master/Update-Notifier-GitHub.ps1 -UseBasicParsing -OutFile .\Update-Notifier-GitHub.ps1
   .\Update-Notifier-GitHub
 }
 else{
-  Write-Warning "No internet connection?... can't ping 1.1.1.1"
+  Write-Warning "No internet connection?... can't ping 9.9.9.9"
 }
 
 # ps.net?
@@ -130,5 +64,5 @@ if($pwsh -ne $Null){
 }
 
 # set location to your safe place
-$location = Set-Location -Path C:\Dev -PassThru
+$location = Set-Location -Path $env:userprofile\Documents\Dev -PassThru
 Write-Host "Setting console location to $($location.Path)..." -ForegroundColor Yellow
